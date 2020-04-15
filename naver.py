885ca86c3f07c2c873dac6785c16756168d698b3
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2020-04-16 00:45:31 +0900

    tellmeit 20200416 - naver.py 선거 후 개편으로 크롤링 관련 수정.

--- naver.py
+++ naver.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 
 def get_crawl_data():
     data_arr = []
-    url = "https://www.naver.com"
-    res = requests.get(url)
+    # 급상승 검색어 상세 옵션은 모두 0으로 세팅한 조건 URL 입니다.
+    url = "https://datalab.naver.com/keyword/realtimeList.naver?age=all&entertainment=-2&groupingLevel=0&marketing=-2&news=-2&sports=-2"
+    headers = {'User-Agent' : 'tellmeit'}
+    res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.text, 'html.parser')
-    for ah_item in soup.findAll('li', {'class':'ah_item'}):
-        rank = ah_item.find('span', {'class':'ah_r'}).text
-        title = ah_item.find('span', {'class':'ah_k'}).text
+    for ah_item in soup.findAll('li', {'class':'ranking_item'}):
+        rank = ah_item.find('span', {'class':'item_num'}).text
+        title = ah_item.find('span', {'class':'item_title'}).text
         data_arr.append(title)
         if rank == "20":
             break
